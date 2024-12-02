132074ed2e1b0ba07a186074b53398d9a684b337
Gustavo Schade <126472320+guschade@users.noreply.github.com>
2024-12-02 11:56:54 -0300

    Add files via upload

---
+++ trabalhoB2/FormPrincipal.Designer.cs
@@ -0,0 +1,332 @@
+﻿using System;
+
+namespace trabalhoB2
+{
+    partial class FormPrincipal
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gerenciamentoToolStripMenuItem;
+        private System.Windows.Forms.Button btnProdutos;
+        private System.Windows.Forms.Button btnFornecedores;
+        private System.Windows.Forms.Button btnPedidos;
+        private System.Windows.Forms.DataGridView dataGridViewPrincipal;
+        private System.Windows.Forms.TextBox txtIDProduto;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox txtDataValidade;
+        private System.Windows.Forms.Label lblIDProduto;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblDataValidade;
+        private System.Windows.Forms.Panel panelMenuLateral;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gerenciamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelMenuLateral = new System.Windows.Forms.Panel();
+            this.btnProdutos = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnPedidos = new System.Windows.Forms.Button();
+            this.dataGridViewPrincipal = new System.Windows.Forms.DataGridView();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.lblIDProduto = new System.Windows.Forms.Label();
+            this.txtIDProduto = new System.Windows.Forms.TextBox();
+            this.lblDataValidade = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtDataValidade = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panelMenuLateral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrincipal)).BeginInit();
+            this.panelContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerenciamentoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1091, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gerenciamentoToolStripMenuItem
+            // 
+            this.gerenciamentoToolStripMenuItem.Name = "gerenciamentoToolStripMenuItem";
+            this.gerenciamentoToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.gerenciamentoToolStripMenuItem.Text = "Gerenciamento";
+            // 
+            // panelMenuLateral
+            // 
+            this.panelMenuLateral.Controls.Add(this.btnProdutos);
+            this.panelMenuLateral.Controls.Add(this.btnFornecedores);
+            this.panelMenuLateral.Controls.Add(this.btnPedidos);
+            this.panelMenuLateral.Location = new System.Drawing.Point(16, 33);
+            this.panelMenuLateral.Margin = new System.Windows.Forms.Padding(4);
+            this.panelMenuLateral.Name = "panelMenuLateral";
+            this.panelMenuLateral.Size = new System.Drawing.Size(160, 506);
+            this.panelMenuLateral.TabIndex = 1;
+            // 
+            // btnProdutos
+            // 
+            this.btnProdutos.Location = new System.Drawing.Point(13, 12);
+            this.btnProdutos.Margin = new System.Windows.Forms.Padding(4);
+            this.btnProdutos.Name = "btnProdutos";
+            this.btnProdutos.Size = new System.Drawing.Size(133, 37);
+            this.btnProdutos.TabIndex = 1;
+            this.btnProdutos.Text = "Produtos";
+            this.btnProdutos.UseVisualStyleBackColor = true;
+            this.btnProdutos.Click += new System.EventHandler(this.btnProdutos_Click);
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.Location = new System.Drawing.Point(13, 62);
+            this.btnFornecedores.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(133, 37);
+            this.btnFornecedores.TabIndex = 2;
+            this.btnFornecedores.Text = "Fornecedores";
+            this.btnFornecedores.UseVisualStyleBackColor = true;
+            this.btnFornecedores.Click += new System.EventHandler(this.btnFornecedores_Click);
+            // 
+            // btnPedidos
+            // 
+            this.btnPedidos.Location = new System.Drawing.Point(13, 111);
+            this.btnPedidos.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPedidos.Name = "btnPedidos";
+            this.btnPedidos.Size = new System.Drawing.Size(133, 37);
+            this.btnPedidos.TabIndex = 3;
+            this.btnPedidos.Text = "Pedidos";
+            this.btnPedidos.UseVisualStyleBackColor = true;
+            this.btnPedidos.Click += new System.EventHandler(this.btnPedidos_Click);
+            // 
+            // dataGridViewPrincipal
+            // 
+            this.dataGridViewPrincipal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPrincipal.Location = new System.Drawing.Point(200, 33);
+            this.dataGridViewPrincipal.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewPrincipal.Name = "dataGridViewPrincipal";
+            this.dataGridViewPrincipal.RowHeadersWidth = 51;
+            this.dataGridViewPrincipal.Size = new System.Drawing.Size(827, 246);
+            this.dataGridViewPrincipal.TabIndex = 4;
+            // 
+            // panelContent
+            // 
+            this.panelContent.Controls.Add(this.btnAdicionar);
+            this.panelContent.Controls.Add(this.btnAtualizar);
+            this.panelContent.Controls.Add(this.btnExcluir);
+            this.panelContent.Controls.Add(this.lblIDProduto);
+            this.panelContent.Controls.Add(this.txtIDProduto);
+            this.panelContent.Controls.Add(this.lblDataValidade);
+            this.panelContent.Controls.Add(this.txtNomeProduto);
+            this.panelContent.Controls.Add(this.lblPreco);
+            this.panelContent.Controls.Add(this.txtQuantidade);
+            this.panelContent.Controls.Add(this.lblQuantidade);
+            this.panelContent.Controls.Add(this.txtPreco);
+            this.panelContent.Controls.Add(this.lblNomeProduto);
+            this.panelContent.Controls.Add(this.txtDataValidade);
+            this.panelContent.Location = new System.Drawing.Point(200, 308);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(4);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(827, 185);
+            this.panelContent.TabIndex = 15;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(396, 86);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(100, 28);
+            this.btnAdicionar.TabIndex = 15;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(504, 86);
+            this.btnAtualizar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(100, 28);
+            this.btnAtualizar.TabIndex = 16;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(612, 86);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 28);
+            this.btnExcluir.TabIndex = 17;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // lblIDProduto
+            // 
+            this.lblIDProduto.AutoSize = true;
+            this.lblIDProduto.Location = new System.Drawing.Point(17, 17);
+            this.lblIDProduto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIDProduto.Name = "lblIDProduto";
+            this.lblIDProduto.Size = new System.Drawing.Size(20, 16);
+            this.lblIDProduto.TabIndex = 10;
+            this.lblIDProduto.Text = "ID";
+            // 
+            // txtIDProduto
+            // 
+            this.txtIDProduto.Location = new System.Drawing.Point(20, 37);
+            this.txtIDProduto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIDProduto.Name = "txtIDProduto";
+            this.txtIDProduto.Size = new System.Drawing.Size(132, 22);
+            this.txtIDProduto.TabIndex = 5;
+            this.txtIDProduto.TextChanged += new System.EventHandler(this.txtIDProduto_TextChanged);
+            // 
+            // lblDataValidade
+            // 
+            this.lblDataValidade.AutoSize = true;
+            this.lblDataValidade.Location = new System.Drawing.Point(580, 17);
+            this.lblDataValidade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDataValidade.Name = "lblDataValidade";
+            this.lblDataValidade.Size = new System.Drawing.Size(113, 16);
+            this.lblDataValidade.TabIndex = 14;
+            this.lblDataValidade.Text = "Data de Validade";
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.Location = new System.Drawing.Point(160, 37);
+            this.txtNomeProduto.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(132, 22);
+            this.txtNomeProduto.TabIndex = 6;
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(440, 17);
+            this.lblPreco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(43, 16);
+            this.lblPreco.TabIndex = 13;
+            this.lblPreco.Text = "Preço";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(300, 37);
+            this.txtQuantidade.Margin = new System.Windows.Forms.Padding(4);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(132, 22);
+            this.txtQuantidade.TabIndex = 7;
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(297, 17);
+            this.lblQuantidade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(77, 16);
+            this.lblQuantidade.TabIndex = 12;
+            this.lblQuantidade.Text = "Quantidade";
+            this.lblQuantidade.Click += new System.EventHandler(this.lblQuantidade_Click);
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(440, 37);
+            this.txtPreco.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(132, 22);
+            this.txtPreco.TabIndex = 8;
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Location = new System.Drawing.Point(160, 17);
+            this.lblNomeProduto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(44, 16);
+            this.lblNomeProduto.TabIndex = 11;
+            this.lblNomeProduto.Text = "Nome";
+            this.lblNomeProduto.Click += new System.EventHandler(this.lblNomeProduto_Click);
+            // 
+            // txtDataValidade
+            // 
+            this.txtDataValidade.Location = new System.Drawing.Point(580, 37);
+            this.txtDataValidade.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDataValidade.Name = "txtDataValidade";
+            this.txtDataValidade.Size = new System.Drawing.Size(132, 22);
+            this.txtDataValidade.TabIndex = 9;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1091, 554);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.dataGridViewPrincipal);
+            this.Controls.Add(this.panelMenuLateral);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormPrincipal";
+            this.Text = "Gerenciamento de Estoque";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelMenuLateral.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPrincipal)).EndInit();
+            this.panelContent.ResumeLayout(false);
+            this.panelContent.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ trabalhoB2/FormPrincipal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace trabalhoB2
+{
+    public partial class FormPrincipal : Form
+    {
+        private MySqlConnection connection;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+            connection = DatabaseConnection.Instance.Connection;
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Produtos", connection);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridViewPrincipal.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar dados: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void btnProdutos_Click(object sender, EventArgs e)
+        {
+            FormProdutos formProdutos = new FormProdutos();
+            formProdutos.MdiParent = this;
+            formProdutos.Show();
+        }
+
+        private void btnFornecedores_Click(object sender, EventArgs e)
+        {
+            FormFornecedores formFornecedores = new FormFornecedores();
+            formFornecedores.MdiParent = this;
+            formFornecedores.Show();
+        }
+
+        private void btnPedidos_Click(object sender, EventArgs e)
+        {
+            FormPedidos formPedidos = new FormPedidos();
+            formPedidos.MdiParent = this;
+            formPedidos.Show();
+        }
+
+        private void txtIDProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNomeProduto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblQuantidade_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
